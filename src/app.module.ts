a4943df4a7077374fc2ae6e7f10f79bd912b0f1b
houcemhamed <housemhamed@gmail.com>
2020-03-11 23:18:17 +0100

    deuxieme version de app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { meschequesmodule } from "./mescheques/mescheque.module";
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule, meschequesmodule ],
   controllers: [AppController],
   providers: [AppService],
 })
